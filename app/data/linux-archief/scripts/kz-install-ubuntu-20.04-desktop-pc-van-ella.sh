697b68d595a705fd2a0d41da4cc8eae9b22d4373
Karel Zimmer <karel.zimmer@gmail.com>
2022-02-14 08:44:44 +0100

    Ubuntu back from archive

--- app/data/linux-archief/scripts/kz-install-ubuntu-20.04-desktop-pc-van-ella.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck shell=bash
-###############################################################################
-# Installatiebestand voor Ubuntu 20.04 LTS desktop op pc-van-ella.            #
-#                                                                             #
-# Geschreven door Karel Zimmer <info@karelzimmer.nl>.                         #
-###############################################################################
-
-#1-(geen opdrachten om te installeren)
-:
