e867113f53f19317553bd1adbdb17a086d7c6e79
Karel Zimmer <karel.zimmer@gmail.com>
2022-10-31 10:07:22 +0100

    Removed Linux Archief from the website

--- app/data/linux-archief/scripts/kz-install-ubuntu-20.04-desktop-pc-van-ella.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck shell=bash
-###############################################################################
-# Installatiebestand voor Ubuntu 20.04 LTS desktop op pc-van-ella.            #
-#                                                                             #
-# Geschreven door Karel Zimmer <info@karelzimmer.nl>.                         #
-###############################################################################
-
-#1-no-apps (geen apps specifiek voor deze computer)
-:
